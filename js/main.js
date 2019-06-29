3fd1d7809cf50218e5060e376185f931c2072125
unknown <jerrardsmith215@gmail.com>
2019-06-29 02:30:58 -0400

    header completed

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// DOM code
+
+// Square up header
+/*const header = document.querySelector('.header'); // target header
+console.log('header element is: ')
+console.log(header)
+const headerWidth = header.clientWidth; // target header width
+console.log('header width is: ' + headerWidth + 'px');
+const newHeight = headerWidth/4; // set new height as 1/4 of header width
+header.style.height = `${newHeight}px`;
+console.log('header height is: ' + header.style.height); // Announce new height of header
+
+// Let image be a perfect square within header
+const headerImg = header.children[0]; // select headshot
+console.log('headshot element is:');
+console.log(headerImg); 
+ // target width
+
+// code ot manipulate width of greeting text depending on width of headshot
+// 1. target the greeting box
+// 2. target header box
+// 4. have the width of the greeting box be the width of the header minus the head shot
