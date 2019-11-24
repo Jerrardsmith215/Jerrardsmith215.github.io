f38ba85921d2d925e7b137294bd9a48cdd11dcab
unknown <jerrardsmith215@gmail.com>
2019-11-24 02:25:21 -0500

    Fixed responsiveness, tweaked fonts and hr tags worked on flex spacing

--- js/main.js
+++ js/main.js
@@ -1,22 +1,9 @@
-// DOM code
+const privacyModal = document.querySelector(".policy")
 
-// Square up header
-/*const header = document.querySelector('.header'); // target header
-console.log('header element is: ')
-console.log(header)
-const headerWidth = header.clientWidth; // target header width
-console.log('header width is: ' + headerWidth + 'px');
-const newHeight = headerWidth/4; // set new height as 1/4 of header width
-header.style.height = `${newHeight}px`;
-console.log('header height is: ' + header.style.height); // Announce new height of header
+function clickHandler() {
+    privacyModal.style.display = "flex";
+}
 
-// Let image be a perfect square within header
-const headerImg = header.children[0]; // select headshot
-console.log('headshot element is:');
-console.log(headerImg); 
- // target width
-
-// code ot manipulate width of greeting text depending on width of headshot
-// 1. target the greeting box
-// 2. target header box
-// 4. have the width of the greeting box be the width of the header minus the head shot
+function clickHandler2() {
+    privacyModal.style.display = "none";
+}
